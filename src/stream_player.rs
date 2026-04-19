83735b0ff6ea59aacc3a8476b872a6f10ac7bd41
Dusan Miloradovic <dusan.miloradovic@gmail.com>
2026-04-19 11:40:33 +0200

    stream player trait

--- src/stream_player.rs
+++ src/stream_player.rs
@@ -1,7 +1,7 @@
-use std::sync::mpsc;
-use cpal::{default_host, StreamError};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use ringbuf::{HeapRb, traits::*, HeapCons, HeapProd};
+use cpal::{default_host, StreamError};
+use ringbuf::{traits::*, HeapProd, HeapRb};
+use std::sync::mpsc;
 
 pub trait StreamPlayer {
     fn new(audio_sample_rate: u32, channels: u16) -> Self;
@@ -16,8 +16,7 @@ struct StreamPlayerImpl {
     channels: u16,
     stream: Option<cpal::Stream>,
     stop_channel_sender: Option<mpsc::Sender<()>>,
-    consumer: HeapCons<f32>,
-    producer: HeapProd<f32>,
+    producer: Option<HeapProd<f32>>,
 }
 
 impl StreamPlayer for StreamPlayerImpl {
@@ -25,15 +24,14 @@ impl StreamPlayer for StreamPlayerImpl {
         let host = cpal::default_host();
         let default_device = host.default_output_device().unwrap();
         let default_config = default_device.default_output_config().unwrap();
-        let (producer, consumer) = HeapRb::<f32>::new(1024).split();
         Self {
             audio_sample_rate,
             channels,
             default_sample_rate: default_config.sample_rate(),
             stream: None,
             stop_channel_sender: None,
-            consumer,
-            producer,
+            producer:None,
+
         }
     }
     fn stop(&mut self) {
@@ -47,26 +45,43 @@ impl StreamPlayer for StreamPlayerImpl {
         self.stop_channel_sender = None;
     }
     fn start(&mut self) {
-        let data_callback = |out: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        let (producer, mut consumer) = HeapRb::<f32>::new(1024).split();
+        self.producer = Some(producer);
+        let data_callback = move |out: &mut [f32], _: &cpal::OutputCallbackInfo| {
             for sample in out.iter_mut() {
-                *sample = 0.0f32;
+                if consumer.is_empty() {
+                    *sample = 0.0f32;
+                } else {
+                    if let Some(s) = consumer.try_pop() {
+                        *sample = s;
+                    } else {
+                        *sample = 0.0f32;
+                    }
+                }
             }
         };
         let err_fn = |err: StreamError| eprintln!("an error occurred on stream: {err}");
         let host = default_host();
-        let device = host.default_output_device().expect("no output device found");
-        let config = device.default_output_config().expect("no default output config");
-        let stream = device.build_output_stream(&config.into(), data_callback, err_fn, None).expect("failed to build stream");
+        let device = host
+            .default_output_device()
+            .expect("no output device found");
+        let config = device
+            .default_output_config()
+            .expect("no default output config");
+        let stream = device
+            .build_output_stream(&config.into(), data_callback, err_fn, None)
+            .expect("failed to build stream");
         stream.play().expect("failed to start stream");
         self.stream = Some(stream);
         let (stop_channel_sender, stop_channel_receiver) = mpsc::channel::<()>();
         self.stop_channel_sender = Some(stop_channel_sender);
         stop_channel_receiver.recv().unwrap();
-
     }
 
     fn push_samples(&mut self, sample: &[f32]) {
         // TODO samples rate adjust with Rubato
-        todo!()
+        if let Some(producer) = self.producer.as_mut() {
+            producer.push_slice(sample);
+        }
     }
 }
